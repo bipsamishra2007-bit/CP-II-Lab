bf433c41cc0bec8054e452b363ee8a5d69b365ae
bipsamishra2007-bit <bipsamishra2007@gmail.com>
2026-01-30 15:14:02 +0530

    Add files via upload

---
+++ Exp 1.1.c
@@ -0,0 +1,12 @@
+//write a program to take a string as input and print it.
+#include<stdio.h>
+void printstring(char s[])
+{
+  int i;
+  for(i=0; s[i]!=0;i++)	
+  printf("%c",s[i]);
+}
+main()
+{
+	printstring("Hello World");
+}
---
+++ Exp 1.3.c
@@ -0,0 +1,13 @@
+// Write a program to reverse a given string and display it
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char a[100];
+	printf("enter a string");
+	scanf("%s", a);
+	strrev(a);
+    printf("reversed string: %s",a);
+	return 0;	
+}
+
