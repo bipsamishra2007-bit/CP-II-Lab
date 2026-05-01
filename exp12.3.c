5f3af86fb6a6e5e3565e1820b40e939403ee8c11
bipsamishra2007-bit <bipsamishra2007@gmail.com>
2026-05-01 14:12:05 +0530

    Add files via upload

---
+++ exp12.3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[] = "apple";
+    char str2[] = "banana";
+
+    int res = strcmp(str1, str2);
+
+    if(res == 0)
+        printf("Strings are equal");
+    else
+        printf("Strings are not equal");
+
+    return 0;
+}
